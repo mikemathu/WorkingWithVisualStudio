fa031ab316a47cb6f9dd179f9c6a38c67b1446ba
Michael Mathu <mathumichael17@gmail.com>
2022-06-04 07:49:22 +0300

    controller and view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+            => View(SimpleRepository.SharedRepository.Products);
+        
+
+       
+    }
+}
